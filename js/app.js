fc171442640a88458869cf6c1b40c744c61cab76
PaulineZeziola <paulinez@wanadoo.fr>
2022-03-02 23:24:28 +0100

    cars list and footer ok

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 new Vue ({
   el:"#app",
   data: {
-    logo: './assets/JScars.png',
+    logo: './assets/images/JScars.png',
     message: 'coucou',
     cars: {
       car1: {
@@ -9,20 +9,47 @@ new Vue ({
         brand: 'Mercedes',
         mileage: '186000',
         price: '9500',
+        picture: './assets/images/mercedesC.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
       },
       car2: {
-        title: 'mercedes classe C',
-        brand: 'Mercedes',
-        mileage: '186000',
-        price: '9500',
+        title: 'fiat 500',
+        brand: 'Fiat',
+        mileage: '52000',
+        price: '11200',
+        picture: './assets/images/fiat500.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
       },
       car3: {
-        title: 'mercedes classe C',
-        brand: 'Mercedes',
-        mileage: '186000',
-        price: '9500',
+        title: 'Citroen C3',
+        brand: 'Citroen',
+        mileage: '89000',
+        price: '7450',
+        picture: './assets/images/citroenC3.jpeg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
+      },
+      car4: {
+        title: 'kangoo',
+        brand: 'Renault',
+        mileage: '256000',
+        price: '3100',
+        picture: './assets/images/kangoo.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
+      },
+      car5: {
+        title: 'kia niro',
+        brand: 'Kia',
+        mileage: '112000',
+        price: '6400',
+        picture: './assets/images/kiaNiro.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
+      },
+      car6: {
+        title: 'Peugeot 206 sw',
+        brand: 'Peugeot',
+        mileage: '145000',
+        price: '3150',
+        picture: './assets/images/206.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
       }
     }
