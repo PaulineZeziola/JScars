1e488e3cbbce2314aba55678f925646aade3aec9
PaulineZeziola <paulinez@wanadoo.fr>
2022-03-02 22:22:26 +0100

    header ok

---
+++ js/app.js
@@ -0,0 +1,30 @@
+new Vue ({
+  el:"#app",
+  data: {
+    logo: './assets/JScars.png',
+    message: 'coucou',
+    cars: {
+      car1: {
+        title: 'mercedes classe C',
+        brand: 'Mercedes',
+        mileage: '186000',
+        price: '9500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
+      },
+      car2: {
+        title: 'mercedes classe C',
+        brand: 'Mercedes',
+        mileage: '186000',
+        price: '9500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
+      },
+      car3: {
+        title: 'mercedes classe C',
+        brand: 'Mercedes',
+        mileage: '186000',
+        price: '9500',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet tempor euismod. Cras nec vehicula magna. Quisque feugiat metus ligula, non ultrices tellus finibus sit amet. Cras posuere felis nec arcu hendrerit, in vehicula lectus tempor. Nulla non pulvinar magna. Donec mollis porta ligula, quis feugiat metus.'
+      }
+    }
+  }
+})
